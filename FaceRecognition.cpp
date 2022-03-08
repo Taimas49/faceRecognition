8e5af17c867e8964dfa7b2a85a05c4c5559cc772
Taimas <urazgalit@gmail.com>
2022-03-08 18:29:35 +0200

    Added mysql db

--- FaceRecognition.cpp
+++ FaceRecognition.cpp
@@ -8,45 +8,101 @@
 #include <opencv2/objdetect.hpp>
 #include <windows.h>
 #include <sys/stat.h>
+#include <string>
+#include <mysql.h>
+
 using namespace cv;
 using namespace std;
 
 
 int main()
 {
+	int qstate;
+
+
+	char imgPath[] = "C:/Users/samti/source/repos/FaceRecognition/FaceRecognition/x64/Debug/1.jpeg";
+	ifstream fileImg(imgPath, ios::binary);
+	fileImg.seekg(0, std::ios::end);
+	int bufferLength = fileImg.tellg();
+	fileImg.seekg(0, std::ios::beg);
+
+	if (fileImg.fail())
+	{
+		cout << "Failed to read image" << endl;
+		cin.get();
+		return -1;
+	}
+
+	//Read image data into char array
+	char* buffer = new char[bufferLength];
+	fileImg.read(buffer, bufferLength);
+	cout << buffer;
+
+	MYSQL* conn;
+	MYSQL_ROW row;
+	MYSQL_RES* result;
+	setlocale(LC_ALL, "ru");
+	conn = mysql_init(0);
+	conn = mysql_real_connect(conn, "localhost", "root", "assasin007", "hightemperaturefaces", 3306, NULL, 0);
+
+	
+
+
+
+
 	double scale = 3.0;
 	CascadeClassifier faceCascade;
 	faceCascade.load("C:\\Users\\samti\\source\\repos\\FaceRecognition/FaceRecognition/haarcascade/haarcascade_frontalface_default.xml");
 
 	VideoCapture cap(0);
 
-	int temperature = 36;
-	Scalar tempColor;
+	int temperature = 40;
 	Scalar goodTemperature = *new Scalar(0, 255, 0);
+	Scalar tempColor = goodTemperature;
 	Scalar InterTemperature = *new Scalar(0, 153, 255);
 	Scalar BadTemperature = *new Scalar(0, 0, 255);
 
+	//char imgPath[] = "C:/Users/samti/source/repos/FaceRecognition/FaceRecognition/1.jpeg";
+	//ifstream fileImg(imgPath, ios::binary);
+
+	//std::ifstream file("1.jpeg");
+	std::vector<char> data;
+
+	fileImg >> std::noskipws;
+	std::copy(std::istream_iterator<char>(fileImg), std::istream_iterator<char>(), std::back_inserter(data));
+
+	//Mat matrixJprg = imdecode(Mat(data), 1);
+
+	//namedWindow("Image from Char Array", WINDOW_AUTOSIZE);
+	//if (!(matrixJprg.empty()))
+	//{
+	//	
+	//	imshow("Image from Char Array", matrixJprg);
+	//}
+
 	while (true)
 	{
-		// Импровизированный датчик температуры
+		// //Импровизированный датчик температуры
 		//while (true)
 		//{
 		//	temperature = 35 + rand() % 6;
 		//	break;
 		//}
 
-		if (temperature <= 36)
-		{
-			tempColor = goodTemperature;
-		}
-		else if (temperature > 36 && temperature < 38)
-		{
-			tempColor = InterTemperature;
-		}
-		else if (temperature > 38)
-		{
-			tempColor = BadTemperature;
-		}
+		//if (temperature <= 36)
+		//{
+		//	tempColor = goodTemperature;
+		//}
+		//else if (temperature > 36 && temperature < 38)
+		//{
+
+		//	tempColor = InterTemperature;
+		//}
+		//else if (temperature > 38)
+		//{
+		//	tempColor = BadTemperature;
+		//}
+
 
 		Mat img;
 		cap >> img;
@@ -70,14 +126,47 @@ int main()
 		imshow("Temperature", img);
 		waitKey(1);
 
-		
+		if (temperature >= 36)
+		{
+
+			if (!conn)
+			{
+				// Если нет возможности установить соединение с сервером
+				// базы данных выводим сообщение об ошибке
+				fprintf(stderr, "Error: can't connect to database %s\n", mysql_error(conn));
+			}
+			else
+			{
+				// Если соединение успешно установлено выводим фразу - "Success!"
+				fprintf(stdout, "Success!\n");
+			}
+
+			char querry[50] = "insert into face (facetemp) values (40)";
+			//string insertQuerry = "insert into person (first_name) values ('TESTTEST2')";
+			//const char* q = insertQuerry.c_str();
+			qstate = mysql_query(conn, querry);
+
+			if (!qstate)
+			{
+				cout << endl << "Row affected";
+			}
+
+			else
+			{
+				cout << "GG";
+			}
+
+
+
+
+
+
+
+			imwrite("1.jpeg", img);
+		}
 	}
-
-
 	return 0;
 }
-
-
 
 
 
@@ -86,7 +175,8 @@ int main()
 /*
 
 
- //Если t > то скрин и заливает в бд
+ Если t > то скрин и заливает в бд
+
 		if (temperature >= 36)
 		{
 		}
@@ -105,6 +195,7 @@ int main()
 		++counter;
 	}
 	string jpeg_name = to_string(counter) + ".jpeg";
+
 
 
 
